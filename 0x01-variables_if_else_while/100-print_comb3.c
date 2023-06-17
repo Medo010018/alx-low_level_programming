148871e38eadea470a9aabaa162d7709dcbf5e89
mohammed <mohammedsayed.ms32@gmail.com>
2023-06-17 00:09:30 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints all possible different combinations of two digits'
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
